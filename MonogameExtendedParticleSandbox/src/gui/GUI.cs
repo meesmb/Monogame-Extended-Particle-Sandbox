cf531a9ad80cb5689e5aa62ff7918360d645dfa9
meesmb <meesbarnhoorn@gmail.com>
2022-11-01 16:44:12 +0100

    added color picker to velocityColorModifier

--- MonogameExtendedParticleSandbox/src/gui/GUI.cs
+++ MonogameExtendedParticleSandbox/src/gui/GUI.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
 using MonoGame.Extended.Particles.Profiles;
+using Myra.Graphics2D.TextureAtlases;
 using Myra.Graphics2D.UI;
+using Myra.Graphics2D.UI.ColorPicker;
 using Myra.Utility;
 
 namespace MonogameExtendedParticleSandbox.src.gui
@@ -48,6 +53,48 @@ namespace MonogameExtendedParticleSandbox.src.gui
             };
             parent.Widgets.Add(button);
             return button;
+        }
+
+        public static ColorPickerDialog createColorPicker(Grid parent, string name, int row, int column, Color defaultColor, Action<HslColor> callback)
+        {
+            var button = new ImageTextButton()
+            {
+                GridRow = row,
+                GridColumn = column,
+                TextColor = defaultColor,
+                Text = name,
+            };
+
+            var colorPicker = new ColorPickerDialog()
+            {
+                Title = name,
+                GridRow = row,
+                GridColumn = column + 1,
+            };
+
+            parent.AddChild(button);
+
+            button.PressedChanged += (s, e) =>
+            {
+                if (button.IsPressed)
+                {
+                    var shouldOpen = !parent.GetChildren().Contains(colorPicker);
+                    if (shouldOpen)
+                        parent.AddChild(colorPicker);
+                    else
+                        colorPicker.Close();
+                }
+            };
+
+            colorPicker.ArrangeUpdated += (s, e) =>
+            {
+                var hsl = HslColor.FromRgb(colorPicker.Color);
+                button.TextColor = colorPicker.Color;
+                callback(hsl);
+            };
+            colorPicker.Color = defaultColor;
+
+            return colorPicker;
         }
 
         public static void createDualSpinButton(Grid parent, int row, int column, string name,
--- MonogameExtendedParticleSandbox/src/gui/modifiers/VelocityColorModifierWidget.cs
+++ MonogameExtendedParticleSandbox/src/gui/modifiers/VelocityColorModifierWidget.cs
@@ -3,31 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
 using MonoGame.Extended.Particles;
 using MonoGame.Extended.Particles.Modifiers;
 using Myra.Graphics2D.UI;
+using Myra.Graphics2D.UI.ColorPicker;
 
 namespace MonogameExtendedParticleSandbox.src.gui.modifiers
 {
     public class VelocityColorModifierWidget : ModifierWidget
     {
+
+        private readonly Color DEFAULT_STATIONARY_COLOR = Color.Green;
+        private readonly Color DEFAULT_VELOCITY_COLOR = Color.Blue;
+        private readonly float DEFAULT_VELOCITY_THRESHOLD = 80;
+
         private VelocityColorModifierWidget(Grid parent, int row, ParticleEmitter emitter) : base(parent, row, emitter)
         {
             var modifier = new VelocityColorModifier();
+
+            modifier.StationaryColor = DEFAULT_STATIONARY_COLOR.ToHsl();
+            modifier.VelocityColor = DEFAULT_VELOCITY_COLOR.ToHsl();
+            modifier.VelocityThreshold = DEFAULT_VELOCITY_THRESHOLD;
+
+
             this.modifier = modifier;
             emitter.Modifiers.Add(modifier);
 
             var text = buildLabel(parent, "Velocity Color", row);
             var grid = buildGrid(parent, row, 3, 1);
 
-            // var rotationRate = GUI.createSpinButton(grid, "Rotation Rate", 0);
-            //
-            // rotationRate.ValueChanged += (e, s) =>
-            // {
-            //     if (s.NewValue != null)
-            //         modifier.RotationRate = (float)s.NewValue;
-            // };
+            var picker1 = GUI.createColorPicker(grid, "Stationary Color", 0, 0, DEFAULT_STATIONARY_COLOR, (hsl) =>
+            {
+                modifier.StationaryColor = hsl;
+            });
 
+            var picker2 = GUI.createColorPicker(grid, "Velocity Color", 1, 0, DEFAULT_VELOCITY_COLOR, (hsl) =>
+            {
+                modifier.VelocityColor = hsl;
+            });
+
+            var velocityThreshold = GUI.createSpinButton(grid, "Velocity Threshold", 2);
+            velocityThreshold.ValueChanged += (s, e) =>
+            {
+                modifier.VelocityThreshold = (float)velocityThreshold.Value;
+            };
+            velocityThreshold.Value = DEFAULT_VELOCITY_THRESHOLD;
         }
 
         public VelocityColorModifierWidget() : base(null, 0, null)
